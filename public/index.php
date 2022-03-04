cf0d89d39e77daca9cdd3319e03a0c1ad8babbdd
ptcheng0812 <ptcheng0812@outlook.com>
2022-03-04 20:54:20 +0800

    Procfile for Apache and PHP

--- public/index.php
+++ public/index.php
@@ -28,5 +28,5 @@ $router = new Core\Router();
 // Add the routes
 $router->add('', ['controller' => 'Home', 'action' => 'index']);
 $router->add('{controller}/{action}');
-    
+
 $router->dispatch($_SERVER['QUERY_STRING']);
